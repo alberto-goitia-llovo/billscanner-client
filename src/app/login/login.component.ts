60d760540e23f7cd2b5b0c7bb703917d51a25aa0
Alberto Goitia <goitia.alberto.ll@gmail.com>
2022-03-28 17:37:55 +0200

    working on login form (issues with p-password primeng component )

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,62 +3,71 @@ import { ConfigService } from '../../services/app.config.service';
 import { AppConfig } from '../../interfaces/appconfig';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styles: [`
-    :host ::ng-deep .p-password input {
-    width: 100%;
-    padding:1rem;
-    }
+      :host ::ng-deep .p-password input {
+        width: 100%;
+        padding:1rem;
+      }
 
-    :host ::ng-deep .pi-eye{
-      transform:scale(1.6);
-      margin-right: 1rem;
-      color: var(--primary-color) !important;
-    }
-
-    :host ::ng-deep .pi-eye-slash{
-      transform:scale(1.6);
-      margin-right: 1rem;
-      color: var(--primary-color) !important;
-    }
+      :host ::ng-deep p-password div>input {
+        width: 100%;
+        padding:1rem;
+      }
+    
+      :host ::ng-deep .pi-eye{
+        transform:scale(1.6);
+        margin-right: 1rem;
+        color: var(--primary-color) !important;
+      }
+      
+      :host ::ng-deep .pi-eye-slash{
+        transform:scale(1.6);
+        margin-right: 1rem;
+        color: var(--primary-color) !important;
+      }
   `]
 })
 export class LoginComponent implements OnInit, OnDestroy {
-
-  valCheck: string[] = ['remember'];
-
   password: string;
 
   config: AppConfig;
 
+  email: string;
+
   subscription: Subscription;
+
+  //p-password has some styling issues when used in an angular form (the toggleMask icon does not look right)
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   constructor(
     public configService: ConfigService,
-    public router: Router
+    public router: Router,
+    public authService: AuthService
   ) { }
 
   ngOnInit(): void {
     // this.config = this.configService.config;
     this.config = this.configService.getConfig();
-    console.log('this.config', this.config)
     this.subscription = this.configService.configUpdate$.subscribe(config => {
       this.config = config;
-      console.log("LA CONFIG HA CAMBIADOO EN EL LOGIIN", this.config);
-      console.log("LA DEL SERVICIO ES", this.configService.getConfig())
     });
   }
 
   ngOnDestroy(): void {
-    // if (this.subscription) {
-    //   this.subscription.unsubscribe();
-    // }
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   signin() {
-    console.log("nos vamos a ", 'pages')
     this.router.navigate(['pages']);
   }
 
